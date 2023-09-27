f73cb600720fb02933ba4bab602bdfd94f86be08
Emiliano <emilianomachado967@gmail.com>
2023-09-27 00:36:26 -0500

    first commit

---
+++ app/Processes/Content.tsx
@@ -0,0 +1,30 @@
+import Form from '@/app/Contact/ContactForm'
+
+export default function Content(){
+    return(
+        <div className="relative min-h-[300px]">
+      <div className="bg-transparent py-10">
+      <div className="container mx-auto">
+        <h1 className="text-3xl font-light mb-6 text-[#4A60A1] text-center uppercase">
+        BUSSINESS PROCESSES
+        </h1>
+
+        <div className="text-lg text-black p-4 text-left">
+        At GPS, excellence in every step of the process is our promise. Although we are a new company, our team has over 15 years of experience in the field. We start by listening carefully to your needs and visions, bringing each idea to life through advanced technology and expert experience.
+          <br />
+          <br />
+          Our team works passionately to create customized solutions that not only meet your expectations, but far exceed them. From meticulous planning to flawless execution, every detail is taken care of with the goal of providing you with exceptional results. Your satisfaction is our priority, and every project is an opportunity to demonstrate our commitment to quality and innovation.
+          <br />
+          <br />
+          With GPS, you are in the hands of dedicated professionals who will transform your concepts into tangible realities. Together, we will make every step of the process an inspiring and rewarding experience. Trust us to make your visions a reality and take your project to the next level. It doesnt matter if its a small or ambitious project; we are here to exceed your expectations and leave a lasting mark on every step we take with you.
+          <br/>
+          <br/>
+          At GPS, excellence in every project is our standard. From planning to execution, we turn your ideas into reality. Ready to start building and transforming? 
+            <br></br><strong>Contact us now and materialize your dreams with us!</strong>
+          </div>
+          <Form></Form>
+            </div>
+          </div>
+          </div>
+    )
+    }
--- app/Processes/page.tsx
+++ app/Processes/page.tsx
@@ -1,44 +1,35 @@
-import bgProcesses from '@/public/Processes.webp';
-import Image from 'next/image';
-import Form from '@/app/Contact/ContactForm'
+"use client"
+import { useState, useEffect } from 'react'
+import Loader from '../components/Loader';
+import Hero from './Hero';
+import Content from './Content';
 
 export default function Processes(){
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setIsLoading(false);
+        observer.disconnect();
+      }
+    }, {
+      threshold: 0,
+    });
+    observer.observe(document.body);
+  }, []);
     return(
-        <main>
-             <div className="relative min-h-[300px]">
-        <Image
-          src={bgProcesses}
-          alt="Descripción de la imagen"
-          fill
-          objectFit="cover"
-          objectPosition="center"
-        />
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white bg-opacity-30 backdrop-blur-lg">
-        </div>
-      </div>
-      <div className="bg-transparent py-10">
-      <div className="container mx-auto">
-        <h1 className="text-3xl font-light mb-6 text-[#4A60A1] text-center uppercase">
-        BUSSINESS PROCESSES
-        </h1>
-
-        <div className="text-lg text-black p-4 text-left">
-        At GPS, excellence in every step of the process is our promise. Although we are a new company, our team has over 15 years of experience in the field. We start by listening carefully to your needs and visions, bringing each idea to life through advanced technology and expert experience.
-          <br />
-          <br />
-          Our team works passionately to create customized solutions that not only meet your expectations, but far exceed them. From meticulous planning to flawless execution, every detail is taken care of with the goal of providing you with exceptional results. Your satisfaction is our priority, and every project is an opportunity to demonstrate our commitment to quality and innovation.
-          <br />
-          <br />
-          With GPS, you are in the hands of dedicated professionals who will transform your concepts into tangible realities. Together, we will make every step of the process an inspiring and rewarding experience. Trust us to make your visions a reality and take your project to the next level. It doesnt matter if its a small or ambitious project; we are here to exceed your expectations and leave a lasting mark on every step we take with you.
-          <br/>
-          <br/>
-          At GPS, excellence in every project is our standard. From planning to execution, we turn your ideas into reality. Ready to start building and transforming? 
-        <br></br><strong>Contact us now and materialize your dreams with us!</strong>
-          </div>
-          <Form></Form>
-              </div>
-          </div>
-
-        </main>
+      <>
+      {
+         isLoading ?  <Loader></Loader> : 
+         <div>
+           <main>
+           <Hero></Hero>
+           <Content></Content>
+           </main>
+         </div>
+       }
+      </>
+        
     )
 }
